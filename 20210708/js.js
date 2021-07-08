ad14dc0c13654ef99935e131188f4e2405ed7034
Loki Jiang <summer10920@gmail.com>
2021-07-08 16:18:45 +0800

    20210708 done

--- 20210708/js.js
+++ 20210708/js.js
@@ -1,7 +1,8 @@
 //宣告區
 const
   btn = document.querySelector("button"),
-  rabbits = document.getElementsByTagName("img");
+  rabbits = document.getElementsByTagName("img"),
+  clrRY = new Array();
 let
   timeNode = document.getElementById("time"),
   countNode = document.getElementById("count"),
@@ -13,7 +14,7 @@ const
   // 遊戲開始
   gamestart = () => {
     //初始
-    timeNode.textContent = sec;
+    timeNode.textContent = sec = 60;
     countNode.textContent = count = 0;
     btn.removeEventListener('click', gamestart);//避免觸發按鈕
 
@@ -44,19 +45,30 @@ const
 
   //圖片替換出現作業
   showOn = (siwho, siwhere, sihow) => {
-    // console.log(`編號${siwho}：位置${siwhere}、停留${sihow}`);
-    rabbits[siwhere].src = "red.png";
-    rabbits[siwhere].alt = siwho;
-    rabbits[siwhere].style.backgroundColor = "red";
 
+    if (rabbits[siwhere].style.backgroundColor == "") { // 該位置必須是黃色，才可允許轉紅色
+      // console.log(`編號${siwho}：位置${siwhere}、停留${sihow}`);
+      rabbits[siwhere].src = "red.png";
+      rabbits[siwhere].alt = siwho;
+      rabbits[siwhere].style.backgroundColor = "red";
 
-    setTimeout(() => { // 在sihow秒之後，將圖片改回黃色並清除style屬性
-      rabbits[siwhere].src = "yellow.png";
-      // rabbit[siwhere].style.backgroundColor = null;
-      rabbits[siwhere].removeAttribute('style');
-      rabbits[siwhere].removeAttribute('alt');
+      //red to yellow, you need keep this serial number
+      clrRY[siwho] = setTimeout(() => { // 在sihow秒之後，將圖片改回黃色並清除style屬性
+        rabbits[siwhere].src = "yellow.png";
+        // rabbit[siwhere].style.backgroundColor = null;
+        rabbits[siwhere].removeAttribute('style');
+        rabbits[siwhere].removeAttribute('alt');
+      }, sihow * 1000);
+    } else { //因為目標位置不能塞紅色，讓這個作業改換下一個位置再安排一次
+      //  5->6 , 8->0
+      // const next = (siwhere == 8) ? 0 : siwhere + 1;  //method1
+      const next = (siwhere + 1) % 9; //method2
 
-    }, sihow * 1000);
+      setTimeout(() => {
+        showOn(siwho, next, sihow);
+      }, 100);
+
+    }
   },
 
   //得分計算
@@ -70,11 +82,15 @@ const
       // countNode.textContent = count;
       countNode.textContent = ++count;
 
+      //green to yellow
       setTimeout(() => {
         ri.src = "yellow.png";
         ri.removeAttribute('style');
         ri.removeAttribute('alt');
       }, 1000);
+
+      // clear red to yellow's bomb
+      clearTimeout(clrRY[ri.alt]);
     }
   };
 
@@ -90,4 +106,21 @@ for (let i = 0; i < rabbits.length; i++) {
     getCount(i);
   });
 }
+
+//鍵盤
+window.addEventListener('keydown', function (e) {
+  // console.log(e);
+  switch (e.code) {
+    case "Numpad7": getCount(0); break;
+    case "Numpad8": getCount(1); break;
+    case "Numpad9": getCount(2); break;
+    case "Numpad4": getCount(3); break;
+    case "Numpad5": getCount(4); break;
+    case "Numpad6": getCount(5); break;
+    case "Numpad1": getCount(6); break;
+    case "Numpad2": getCount(7); break;
+    case "Numpad3": getCount(8); break;
+  }
+});
+
 
